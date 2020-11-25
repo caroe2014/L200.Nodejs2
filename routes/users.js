bceaafcac3a999bf86c450b92a045a7308e7250e
caroe2014 <edwincaro10@hotmail.com>
2020-11-25 14:16:16 -0600

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.json([{user: 'Adam', job: 'Global Security'}, {user: 'Chris', job: 'Blesser of Images'}, {user: 'Edwin Caro', job: 'Support Engineer'}]); 
 });
 
